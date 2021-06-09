392215b6531d334c22c9a61a682ca3b47f21df72
Ken <kensikes988@gmail.com>
2021-06-08 20:53:02 -0700

    saving

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -18,4 +18,11 @@ function markerColor(mag) {
       return "#FFA500";
   } else {
       return "#FF0000";
-  }
+  };
+}
+
+// Perform a GET request to the query URL
+d3.json(link, function(data) {
+    // Once we get a response, send the data.features object to the createFeatures function
+    createFeatures(data.features);
+  });
