fa2b471500ffb47d3192a5ac5f036f8a91e2d122
Ken <kensikes988@gmail.com>
2021-06-09 19:14:34 -0700

    saving

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -67,3 +67,11 @@ function createMap(earthquakes) {
     id: "mapbox/light-v10",
     accessToken: API_KEY
 });
+
+// Define outdoors layer 
+var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox/outdoors-v11",
+  accessToken: API_KEY
+});
