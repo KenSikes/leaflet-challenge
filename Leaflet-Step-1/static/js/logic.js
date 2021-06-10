f687a4ecfe87df9e69158cb25c32cee0e77a34ea
Ken <kensikes988@gmail.com>
2021-06-09 18:43:00 -0700

    saving

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,2 +1,8 @@
 //Variable for JSON
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+//GET request to query URL 
+d3.json(url)
+    .then(function (data) {
+        createFeatures(data.features);
+    });
