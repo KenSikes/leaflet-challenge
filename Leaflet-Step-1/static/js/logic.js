4efaad979a0a174b68fc95f8b7b86d11010cb152
Ken <kensikes988@gmail.com>
2021-06-09 19:03:02 -0700

    saving

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -47,3 +47,13 @@ function createFeatures(earthquakeData) {
 
   createMap(earthquakes);
 }
+
+function createMap(earthquakes) { 
+  var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      tileSize: 512,
+      maxZoom: 18,
+      zoomOffset: -1,
+      id: "mapbox/satellite-streets-v11",
+      accessToken: API_KEY
+  });
